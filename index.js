3fe7839eabb726b35e6b1acb42237a70b74f7148
Emilse Arias <nachsalas93@gmail.com>
2020-12-15 18:40:35 -0300

    se creo la primera parte del back end para agregar consultas de pacientes

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const bodyParser = require('body-parser');
+
+// crear el servidor
+const app = express();
+
+// Conectar a mongodb
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/T&SMedicina', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}); 
+
+//habilitar el body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// habilitar routing
+app.use('/', routes())
+
+//puerto y arrancar el servidor
+app.listen(4000, () => {
+    console.log('Servidor funcionando');
+})
